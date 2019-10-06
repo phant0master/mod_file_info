5098fba9b335fe270592a7555f41ebd7c1efa7d4
phant0master <marac123456789@gmail.com>
2019-10-06 15:46:08 +0200

    dev: backup

--- src/mod_info/mod_info.c
+++ src/mod_info/mod_info.c
@@ -4,16 +4,18 @@
 #include <byteswap.h>
 #include "mod_struct.h"
 
-mod_file_header_t header;
+mod_file_t mod_file;
+static unsigned long sample_length_sum = 0;
+static unsigned long file_size = 0;
 
-static void mod_big_endian_to_little(struct mod_sample_info *sample_nfo)
+static void mod_big_endian_to_little(struct sample_info *sample_nfo)
 {
     sample_nfo->sample_length_bytes = __bswap_16(sample_nfo->sample_length_bytes) * 2;
     sample_nfo->repeat_offset = __bswap_16(sample_nfo->repeat_offset) * 2;
     sample_nfo->repeat_length = __bswap_16(sample_nfo->repeat_length) * 2;
 }
 
-static void mod_print_sample_info(struct mod_sample_info *sample_nfo)
+static void mod_print_sample_info(struct sample_info *sample_nfo)
 {
     printf("##################_SAMPLE INFO_##################\n");
     printf("    Sample name:    %s\n", sample_nfo->title);
@@ -25,58 +27,91 @@ static void mod_print_sample_info(struct mod_sample_info *sample_nfo)
     printf("######################_END_######################\n\n");
 }
 
+static void mod_print_one_pattern(struct pattern *_pattern)
+{
+    for(int i = 0; i < ROWS_IN_PATTERN; i++)
+    {
+        printf("    %02x  |   %02x %02x %02x %02x   %02x %02x %02x %02x     %02x %02x %02x %02x     %02x %02x %02x %02x\n",
+            i,
+            _pattern->row[i].channel[0].note[0], _pattern->row[i].channel[0].note[1], _pattern->row[i].channel[0].note[2], _pattern->row[i].channel[0].note[3], 
+            _pattern->row[i].channel[1].note[0], _pattern->row[i].channel[1].note[1], _pattern->row[i].channel[1].note[2], _pattern->row[i].channel[1].note[3],
+            _pattern->row[i].channel[2].note[0], _pattern->row[i].channel[2].note[1], _pattern->row[i].channel[2].note[2], _pattern->row[i].channel[2].note[3],
+            _pattern->row[i].channel[3].note[0], _pattern->row[i].channel[3].note[1], _pattern->row[i].channel[3].note[2], _pattern->row[i].channel[3].note[3]);
+    }
+}
+
 int main(int argc, char **argv)
 {
     FILE *file;
 
-    printf("----------------------Mod file info----------------------\n");
+    printf("#-------------------------------------------------------#\n");
+    printf("#---------------------Mod file info---------------------#\n");
+    printf("#-------------------------------------------------------#\n\n");
+
+    if(argc <= 1)
+    {
+        printf("No file path provided!\n");
+        return -1;
+    }
+
     printf("Loading file: %s\n\n", argv[1]);
 
     if(NULL == (file = fopen(argv[1], "rb")))
     {
         printf("fopen fail!\n");
-        return -1;
+        return -12;
     }
 
-    uint32_t tmp = 0;    
+    // Get file size in bytes
+    fseek(file, 0L, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
 
-    fseek(file, FILE_FORMAT_TAG_OFFSET, SEEK_SET);
-
-    // Read .mod file initials
-    if(0 == fread(&tmp, sizeof(uint32_t), 1, file))
+    printf("File size is: %lu bytes\n", file_size);
+    
+    // Read .mod file mod_file
+    if(0 == fread(&mod_file, sizeof(struct mod_header), 1, file))
     {
         printf("fread fail!\n");
-        return -2;
+        return -3;
     }
 
     // Check if .mod file has a M.K. initials
-    if(tmp == _MK)
-        fseek(file, 0x00, SEEK_SET);
-    else
+    if(mod_file.header.file_format_tag != _MK)
     {
         printf("File has no \"M.K\" initials, exiting...\n");
-        return -3;
-    }
-    
-    // Read .mod file header
-    if(0 == fread(&header, sizeof(mod_file_header_t), 1, file))
-    {
-        printf("fread fail!\n");
         return -4;
     }
 
     for(int i = 0; i < 31; i++)
     {
-        mod_big_endian_to_little(&header.sample_info[i]);
-        mod_print_sample_info(&header.sample_info[i]);
+        mod_big_endian_to_little(&mod_file.header.sample_info[i]);
+        mod_print_sample_info(&mod_file.header.sample_info[i]);
+        sample_length_sum += mod_file.header.sample_info[i].sample_length_bytes;
     }
 
-    printf("\nNumber of patterns: %d\n", header.num_of_patterns);
-    printf("End jump position: %d\n", header.end_pos_jump);
+    printf("\nNumber of patterns: %d\n", mod_file.header.num_of_patterns);
+    printf("End jump position: %d\n", mod_file.header.end_pos_jump);
 
     printf("Patern table: ");
-    for(int i = 0; i < header.num_of_patterns; i++)
-        printf(" %d ", header.pattern_table[i]);
+    for(int i = 0; i < mod_file.header.num_of_patterns; i++)
+        printf(" %d ", mod_file.header.pattern_table[i]);
+    printf("\n");
+
+    unsigned long patterns_length = file_size - sizeof(struct mod_header) - sample_length_sum;
+
+    printf("Sample length sum:  %lu bytes\n", sample_length_sum);
+    printf("Patterns length:    %lu bytes\n", patterns_length);
+
+    printf("Size of pattern: %lu\n", sizeof(struct pattern));
+
+    if(0 == fread(&mod_file.pattern, sizeof(struct pattern), patterns_length / sizeof(struct pattern), file))
+    {
+        printf("fread fail!\n");
+        return -4;
+    }
+
+    mod_print_one_pattern(&mod_file.pattern[0]);
 
     printf("\n");
     return 0;
--- src/mod_info/mod_struct.h
+++ src/mod_info/mod_struct.h
@@ -4,9 +4,12 @@
 #include <stdint.h>
 
 #define _MK 0x2E4B2E4D
-#define FILE_FORMAT_TAG_OFFSET 0x438
+#define FILE_FORMAT_TAG_OFFSET  0x438
+#define BYTES_PER_NOTE  4
+#define NUM_OF_CHANNELS 4
+#define ROWS_IN_PATTERN 64
 
-struct mod_sample_info
+struct sample_info
 {
     char title[22];
     uint16_t sample_length_bytes;
@@ -16,14 +19,35 @@ struct mod_sample_info
     uint16_t repeat_length;
 };
 
-typedef struct mod_file_header_t
+struct note_data
+{
+    uint8_t note[BYTES_PER_NOTE];
+};
+
+struct pattern_row_data
+{
+    struct note_data channel[NUM_OF_CHANNELS];
+};
+
+struct pattern
+{
+    struct pattern_row_data row[ROWS_IN_PATTERN];
+};
+
+struct mod_header
 {
     char song_title[20];
-    struct mod_sample_info sample_info[31];
+    struct sample_info sample_info[31];
     uint8_t num_of_patterns;
     uint8_t end_pos_jump;
     uint8_t pattern_table[128];
+    uint32_t file_format_tag;
+};
 
-}mod_file_header_t;
+typedef struct mod_file_t
+{
+    struct mod_header header;
+    struct pattern pattern[256];
+}mod_file_t;
 
 #endif
